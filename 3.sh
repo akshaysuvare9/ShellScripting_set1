b470b92a4b5a3c5b73df50085a6182a7f736a2d9
akshaysuvare9 <74777762+akshaysuvare9@users.noreply.github.com>
2020-11-27 10:14:42 +0530

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,4 +1,11 @@
    #!/bin/bash
+#greeting:
+#Write a shell script that greets the user on login with:
+#Good Morning, <username> !       OR
+#Good Afternoon, <username> !    OR
+#Good Evening, <username> !
+#depending on the time of day.
+#------------------------------------------------------------------------------------
    hr=$(date +"%H")
    
    if [ $hr -ge 0 -a $hr -lt 12 ]
